066e0c6e0aafc39879e764eac4e52c694cf8d64f
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-12-22 08:44:13 +0530

    Use new style of ref

--- src/parent/parent.component.js
+++ src/parent/parent.component.js
@@ -4,14 +4,15 @@ import Child from '../child/child.container';
 class Parent extends React.Component {
 
     render() {
+        let childRef = null;
         return (
             <div>
                 Parent
             <br />
-                <Child id="child" ref="childRef" />
+                <Child id="child" ref={input => childRef = input} />
                 <br />
                 <br />
-                <input id="btn" type="button" value="Check" onClick={e => this.props.handleClick(this.refs.childRef)} />
+                <input id="btn" type="button" value="Check" onClick={e => this.props.handleClick(childRef)} />
             </div>
         )
     }
